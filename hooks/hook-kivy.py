f9b93a1da1c944af760340f07c633c42c3a0cd24
tianhe69 <13901086621@139.com>
2026-01-17 11:31:11 +0800

    Update Kivy GL backend environment variable
    Change Kivy GL backend from 'sdl2' to 'angle_sdl2' for compatibility.

--- hooks/hook-kivy.py
+++ hooks/hook-kivy.py
@@ -1,4 +1,7 @@
+# PyInstaller hook for Kivy
+# 在 Kivy 导入之前设置环境变量
 import os
-os.environ['KIVY_GL_BACKEND'] = 'sdl2'
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 os.environ['KIVY_WINDOW'] = 'sdl2'
 os.environ['KIVY_AUDIO'] = 'sdl2'
+os.environ['KIVY_VIDEO'] = 'sdl2'
